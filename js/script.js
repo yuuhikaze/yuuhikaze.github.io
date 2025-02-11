f76667622eb9fba26466b51309f5f951d9c26554
yuuhikaze <yuuhikaze@protonmail.com>
2025-02-10 22:07:30 -0500

    feat: adds illustrations

--- js/script.js
+++ js/script.js
@@ -12,3 +12,43 @@ function loadDiagram(event) {
     secondaryPane.innerHTML = ''; // Clear any previous content
     secondaryPane.appendChild(imgElement); // Add the new diagram to the secondary pane
 }
+
+function loadIllustrations(event) {
+    // Prevent default link behavior
+    event.preventDefault();
+
+    // List of illustration images
+    const illustrations = [
+        'static/images/illustrations/2025-02-10_21h54m40s.png',
+        'static/images/illustrations/PXL_20240702_134439701.jpg',
+        'static/images/illustrations/el-pedro-1.png',
+        'static/images/illustrations/2022-12-25_10h31m04s.png',
+        'static/images/illustrations/2023-02-08_08h13m25s.png',
+        'static/images/illustrations/path3376.png',
+        'static/images/illustrations/leaves.png',
+        'static/images/illustrations/apple-cloth-1.png',
+        'static/images/illustrations/mural-sketch.png',
+    ];
+
+    // Select the secondary pane and clear its previous content
+    var secondaryPane = document.getElementById('secondary-pane');
+    secondaryPane.innerHTML = '';
+
+    // Create a gallery container
+    var galleryContainer = document.createElement('div');
+    galleryContainer.classList.add('gallery-container');
+
+    // Loop through the images and create image elements for each
+    illustrations.forEach(function(imagePath) {
+        var imgElement = document.createElement('img');
+        imgElement.src = imagePath;
+        imgElement.alt = "Illustration";
+        imgElement.classList.add('gallery-item');
+        
+        // Add the image to the gallery container
+        galleryContainer.appendChild(imgElement);
+    });
+
+    // Append the gallery container to the secondary pane
+    secondaryPane.appendChild(galleryContainer);
+}
